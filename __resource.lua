deaafcf4ecc0036914b3ee350c2d0302b73a3362
InteLCore-star <67869025+InteLCore-star@users.noreply.github.com>
2020-07-10 20:03:05 +0800

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+-- https://wiki.fivem.net/wiki/Resource_manifest
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Dont Sell This Source'
+
+version '1.1.0'
+
+client_scripts {
+	'client/no_drive_by-client.lua'
+}
